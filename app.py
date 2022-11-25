ee5079a814f1fac39a4c767ea3a42d367409d72d
CKRajan <ckrajan.tv@gmail.com>
2022-11-25 23:48:16 +0530

    second commit

--- app.py
+++ app.py
@@ -1,11 +1,88 @@
-from flask import Flask, render_template, request, make_response
 import os
+import urllib.request
+from flask import Flask, flash, request, redirect, url_for, render_template, make_response
+from werkzeug.utils import secure_filename
+from moviepy.editor import *
+import csv
+
+# if not os.path.exists('static/uploads'):
+# 	os.mkdir('static/uploads/')
+
+UPLOAD_FOLDER = 'static/uploads/'
 
 app = Flask(__name__, static_url_path='/static')
+app.secret_key = "secret key"
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+# app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
 
 @app.route("/")
 def cutter():
     return render_template('cutter.html')
+
+@app.route('/', methods=['POST'])
+def upload_video():
+	collection = request.form.get('collection')
+	if 'file' not in request.files:
+		flash('No file part')
+		return redirect(request.url)
+	file = request.files['file']
+	if file.filename == '':
+		flash('No image selected for uploading')
+		return redirect(request.url)
+	else:
+		filename = secure_filename(file.filename)
+		file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+		flash('Video successfully uploaded and displayed below')
+		return render_template('cutter.html', filename=filename, collection=collection)
+
+@app.route('/upload_csv', methods=['POST'])
+def upload():
+	json_body = request.get_json()
+	csv_str = json_body['csv']
+	filename = json_body['filename']
+	filename_only = os.path.splitext(filename)[0]
+
+	reader = csv.reader(csv_str.splitlines(), skipinitialspace=True)
+	chop_path = 'static/uploads/%s' % filename_only + '/'
+	with open(chop_path + filename_only + '.csv', 'w') as out_file:
+		writer = csv.writer(out_file)
+		writer.writerows(reader)
+
+@app.route('/chop_video', methods=['POST'])
+def chop_video():
+	json_body = request.get_json()
+	rows_final = json_body['rows_final']
+	filename = json_body['filename']
+	filename_only = os.path.splitext(filename)[0]
+	chop_path = 'static/uploads/%s' % filename_only + '/'
+
+	if not os.path.exists('static/uploads/%s' % filename_only):
+		os.mkdir(chop_path)
+
+	for i in rows_final:
+		clip = VideoFileClip(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+		start_time = i[3]
+		end_time = i[4]
+		clip_x = clip.subclip(start_time, end_time)
+		clip_x.write_videofile(os.path.join(chop_path, i[2] + ".mp4"))
+	
+	return filename
+
+@app.route('/display/<filename>')
+def display_video(filename):
+	return redirect(url_for('static', filename='uploads/' + filename), code=301)
+
+# @app.route('/display_server/<filename>')
+# def display_server_video(filename):
+# 	return redirect(url_for('static', filename='uploads/' + filename), code=301)
+
+@app.route('/uploaded_files', methods=['GET', 'POST'])
+def uploaded_files():
+	video_list = 'static/uploads/'
+	allfiles = os.listdir(video_list)
+	files = [ fname for fname in allfiles if fname.endswith('.mp4')]
+	print(files)
+	return files
 
 
 if __name__ == '__main__':
--- static/cutter.js
+++ static/cutter.js
@@ -1,10 +1,11 @@
-
+var collection_name;
 var upload_file;
 let video2;
 
 var video_scr = document.getElementById('video_scr_id');
 var ls = document.getElementById("list");
 var export_csv = document.getElementById("export_csv");
+var chop_upload_id = document.getElementById("chop_upload");
 
 var time_arr = [];
 var press = 0;
@@ -17,6 +18,14 @@ var csv;
 var spliced;
 var clipname;
 var clipname_arr = [];
+var uploaded_file_name;
+var encodedUri;
+var final_csv;
+
+$('#export_csv').click(function () {
+    chop_upload_id.style.display = "block";
+});
+
 
 document.body.onkeyup = function (e) {
     if (e.key == "c" ||
@@ -24,6 +33,10 @@ document.body.onkeyup = function (e) {
         e.keyCode == 67
     ) {
         //your code
+        uploaded_file_name = file_name;
+        collection_name = collection_name_final;
+
+        // collection_name = document.getElementById("collection").value;
         press++;
         export_csv.style.display = "block";
 
@@ -58,13 +71,12 @@ document.body.onkeyup = function (e) {
             console.log(time_arr);
             var start_time = time_arr[0];
             var end_time = time_arr[1];
-            var collection_name = document.getElementById("collection").value;
             split_name++;
             ls.style.display = "block";
             ls.innerHTML += '<div id="box' + counter + '" style="color:rgb(31, 87, 37); font-size: 18px;"><i style="color:red; id="trash' + counter + '" class="fas fa-trash" onclick="removeItem(this);"></i> Collection: ' + collection_name + '; Split name: Clip_' + split_name + '; Start time: ' + start_time + '; End time: ' + end_time + '</div>';
 
             if (counter > 0) {
-                rows[counter] = [upload_file.name, collection_name, "Clip_" + split_name, start_time, end_time];
+                rows[counter] = [uploaded_file_name, collection_name, "Clip_" + split_name, start_time, end_time];
             }
 
             while (time_arr.length > 0) {
@@ -85,6 +97,28 @@ function removeItem(element) {
     element.parentNode.removeChild(element); // will remove the element from DOM
 }
 
+async function send_timings() {
+    const { request } = await axios.post("/chop_video", {
+        rows_final: rows_final,
+        filename: uploaded_file_name,
+        contentType:
+            "text/json"
+    });
+
+    return request
+}
+
+async function send_csv() {
+    const { request } = await axios.post("/upload_csv", {
+        csv: final_csv,
+        filename: uploaded_file_name,
+        contentType:
+            "text/json"
+    });
+
+    return request
+}
+
 function csv_export() {
 
     rows_final = rows.filter(function (val) {
@@ -96,51 +130,104 @@ function csv_export() {
         csv += "\n";
     });
 
-    var final_csv = csv.replace('undefinedFile name', 'File name');
+    final_csv = csv.replace('undefinedFile name', 'File name');
 
     var csvContent = "data:text/csv;charset=utf-8," + final_csv + "\n";
 
-    var encodedUri = encodeURI(csvContent);
+    encodedUri = encodeURI(csvContent);
+
     var link = document.createElement("a");
     link.style.display = "none";
     link.setAttribute("href", encodedUri);
     link.setAttribute("download", "data_collection.csv");
     document.body.appendChild(link); // Required for FF
 
-    link.click(); // This will download the data file named "my_data.csv".
+    link.click(); // This will download the data file named "data_collection.csv".
+
+    var theRemovedElement = rows_final.shift();
 }
 
-$("#upload-button").click(function () {
-    $("#upload").click();
-});
+function chop_upload() {
+    send_timings();
+    send_csv();
+}
 
-$('#upload').change(function () {
-    document.getElementById("upload").disabled = true;
 
-    upload_file = document.getElementById('upload').files[0];
-    let videoMetaData = (upload_file) => {
-        return new Promise(function (resolve, reject) {
-            video_scr.addEventListener('canplay', function () {
-                resolve({
-                    video: video_scr,
-                    height: video_scr.videoHeight,
-                    width: video_scr.videoWidth
-                });
-            });
-            video_scr.src = URL.createObjectURL(upload_file);
-            document.body.appendChild(video_scr);
-            video_scr.style.display = "block";
-            video_scr.muted = false;
-            video_scr.play();
-            video_scr.controls = true;
 
-            video_scr.autoplay = false;
-            video_scr.height = 500;
-            video_scr.width = 800;
-            video_scr.position = "relative";
-            video_scr.style.padding = "5px";
-        })
-    }
-    videoMetaData($('#upload')[0].files[0]).then(function (value) {
-    })
-});
+// This is that input field
+const videoInput = document.getElementById('videoInput');
+
+// This is the datalist
+const datalist = document.getElementById('videoList');
+
+function populateList(arr) {
+    arr.forEach(country => {
+        var option = document.createElement("option");
+        option.innerHTML = country;
+        datalist.appendChild(option);
+    });
+}
+
+var video_list;
+
+setTimeout(() => {
+    var t = JSON.parse(video_list)
+    populateList(t);
+}, 1000);
+
+// setTimeout(() => {
+//     var datalist_value = document.getElementById('videoInput');
+//     var ff = datalist_value.value;
+// }, 2000);
+
+get_uploaded_files()
+    .then(result => video_list = result.responseText)
+    .catch(error => console.error('Video id generated from client', error));
+
+
+async function get_uploaded_files() {
+    const { request } = await axios.post("/uploaded_files", {
+        contentType:
+            "text/json"
+    });
+
+    return request
+}
+
+
+
+// $("#upload-button").click(function () {
+//     $("#upload").click();
+// });
+
+// $('#upload').change(function () {
+//     // document.getElementById("upload").disabled = true;
+
+//     upload_file = document.getElementById('upload').files[0];
+//     upload_file_name = upload_file.name;
+    // let videoMetaData = (upload_file) => {
+    //     return new Promise(function (resolve, reject) {
+    //         video_scr.addEventListener('canplay', function () {
+    //             resolve({
+    //                 video: video_scr,
+    //                 height: video_scr.videoHeight,
+    //                 width: video_scr.videoWidth
+    //             });
+    //         });
+    //         video_scr.src = URL.createObjectURL(upload_file);
+    //         document.body.appendChild(video_scr);
+    //         video_scr.style.display = "block";
+    //         video_scr.muted = false;
+    //         video_scr.play();
+    //         video_scr.controls = true;
+
+    //         video_scr.autoplay = false;
+    //         video_scr.height = 500;
+    //         video_scr.width = 800;
+    //         video_scr.position = "relative";
+    //         video_scr.style.padding = "5px";
+    //     })
+    // }
+    // videoMetaData($('#upload')[0].files[0]).then(function (value) {
+    // })
+// });
